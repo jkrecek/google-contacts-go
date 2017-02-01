af93d050c60c31bb7d7127a8427a006b6ead8e8d
Honza Křeček <killerfrca@gmail.com>
2017-02-01 13:55:37 +0100

    Add support for phones and organizations for contacts
    Signed-off-by: Honza Křeček <killerfrca@gmail.com>

--- entity_contact.go
+++ entity_contact.go
@@ -5,11 +5,16 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"strings"
 	"time"
 )
 
 var (
 	urlIdRegex = regexp.MustCompile(`^.+/([0-9a-f]+)$`)
+)
+
+const (
+	schemaRef = "http://schemas.google.com/g/2005"
 )
 
 type contactFeed struct {
@@ -23,12 +28,15 @@ type contactFeed struct {
 }
 
 type Contact struct {
-	XMLName xml.Name  `xml:"http://www.w3.org/2005/Atom entry"`
-	IdUrl   string    `xml:"id,omitempty"`
-	Updated time.Time `xml:"updated,omitempty"`
-	Title   string    `xml:"title,omitempty"`
-	Name    Name      `xml:"name,omitempty"`
-	Emails  []*Email  `xml:"email,omitempty"`
+	XMLName      xml.Name                   `xml:"http://www.w3.org/2005/Atom entry"`
+	IdUrl        string                     `xml:"id,omitempty"`
+	Updated      time.Time                  `xml:"updated,omitempty"`
+	Title        string                     `xml:"title,omitempty"`
+	Name         Name                       `xml:"name,omitempty"`
+	Organization *Organization              `xml:"organization,omitempty"`
+	Emails       []*Email                   `xml:"email,omitempty"`
+	Phones       []*Phone                   `xml:"phoneNumber,omitempty"`
+	Addresses    []*StructuredPostalAddress `xml:"structuredPostalAddress,omitempty"`
 }
 
 func (c *Contact) GetId() (string, error) {
@@ -42,6 +50,18 @@ func (c *Contact) GetId() (string, error) {
 
 func (c *Contact) SetId(id string) {
 	c.IdUrl = fmt.Sprintf("http://www.google.com/m8/feeds/contacts/default/base/%s", id)
+}
+
+type rel struct {
+	Rel string `xml:"rel,attr"`
+}
+
+func (r *rel) GetType() string {
+	return strings.Replace(r.Rel, schemaRef+"#", "", -1)
+}
+
+func (r *rel) SetType(t string) {
+	r.Rel = schemaRef + "#" + t
 }
 
 type Name struct {
@@ -61,13 +81,69 @@ func NewName(givenName, familyName string) Name {
 
 type Email struct {
 	XMLName xml.Name `xml:"http://schemas.google.com/g/2005 email"`
-	Rel     string   `xml:"rel,attr"`
-	Address string   `xml:"address,attr"`
-	Primary bool     `xml:"primary,attr,omitempty"`
+	rel
+	Address string `xml:"address,attr"`
+	Primary bool   `xml:"primary,attr,omitempty"`
+}
+
+func NewEmail(address string, t string, primary bool) Email {
+	var r rel
+	r.SetType(t)
+
+	return Email{
+		Address: address,
+		rel:     r,
+		Primary: primary,
+	}
 }
 
 type GroupMembershipInfo struct {
 	XMLName xml.Name `xml:"http://schemas.google.com/contact/2008 groupMembershipInfo"`
 	Deleted bool     `xml:"deleted,attr"`
 	Href    string   `xml:"href,attr"` // NOTE default `My contacts` group seems to be `http://www.google.com/m8/feeds/groups/{userEmail}/base/6`
+}
+
+type Phone struct {
+	XMLName xml.Name `xml:"http://schemas.google.com/g/2005 phoneNumber"`
+	rel
+	Number string `xml:",innerxml"`
+}
+
+func NewPhone(number string, t string) Phone {
+	var r rel
+	r.SetType(t)
+
+	return Phone{
+		Number: number,
+		rel:    r,
+	}
+}
+
+type Organization struct {
+	XMLName xml.Name `xml:"http://schemas.google.com/g/2005 organization"`
+	rel
+	OrganizationName  string `xml:"orgName,omitempty"`
+	OrganizationTitle string `xml:"orgTitle,omitempty"`
+}
+
+func NewOrganization(name, title string) Organization {
+	var r rel
+	r.SetType("other")
+	return Organization{
+		OrganizationName:  name,
+		OrganizationTitle: title,
+		rel:               r,
+	}
+}
+
+type StructuredPostalAddress struct {
+	XMLName      xml.Name `xml:"http://schemas.google.com/g/2005 structuredPostalAddress"`
+	Rel          string   `xml:"rel,attr"`
+	Street       string   `xml:"street,omitempty"`
+	POBox        string   `xml:"pobox,omitempty"`
+	Neighborhood string   `xml:"neighborhood,omitempty"`
+	City         string   `xml:"city,omitempty"`
+	Region       string   `xml:"region,omitempty"`
+	Postcode     string   `xml:"postcode,omitempty"`
+	Country      string   `xml:"country,omitempty"`
 }
--- entity_contact_test.go
+++ entity_contact_test.go
@@ -2,6 +2,7 @@ package contacts
 
 import (
 	"encoding/xml"
+	"github.com/davecgh/go-spew/spew"
 	. "gopkg.in/check.v1"
 	"regexp"
 	"testing"
@@ -10,43 +11,63 @@ import (
 
 const (
 	CONTACT_EXAMPLE = `<?xml version="1.0" encoding="UTF-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom" xmlns:batch="http://schemas.google.com/gdata/batch" xmlns:gContact="http://schemas.google.com/contact/2008" xmlns:gd="http://schemas.google.com/g/2005" xmlns:openSearch="http://a9.com/-/spec/opensearchrss/1.0/">
+<feed gd:etag="&quot;SHk7eTVSLyt7I2A9XR5WGUkJRAA.&quot;" xmlns="http://www.w3.org/2005/Atom" xmlns:batch="http://schemas.google.com/gdata/batch" xmlns:gContact="http://schemas.google.com/contact/2008" xmlns:gd="http://schemas.google.com/g/2005" xmlns:openSearch="http://a9.com/-/spec/opensearch/1.1/">
  <id>easycore.sync.bridge@gmail.com</id>
- <updated>2017-01-19T08:54:51.668Z</updated>
+ <updated>2017-02-01T10:02:29.701Z</updated>
  <category scheme="http://schemas.google.com/g/2005#kind" term="http://schemas.google.com/contact/2008#contact"/>
- <title type="text">Easy Core's Contacts</title>
+ <title>Easy Core's Contacts</title>
  <link rel="alternate" type="text/html" href="https://www.google.com/"/>
- <link rel="http://schemas.google.com/g/2005#feed" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/thin"/>
- <link rel="http://schemas.google.com/g/2005#post" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/thin"/>
- <link rel="http://schemas.google.com/g/2005#batch" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/thin/batch"/>
- <link rel="self" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/thin?max-results=25"/>
+ <link rel="http://schemas.google.com/g/2005#feed" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full"/>
+ <link rel="http://schemas.google.com/g/2005#post" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full"/>
+ <link rel="http://schemas.google.com/g/2005#batch" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full/batch"/>
+ <link rel="self" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full?max-results=25"/>
  <author>
   <name>Easy Core</name>
   <email>easycore.sync.bridge@gmail.com</email>
  </author>
  <generator version="1.0" uri="http://www.google.com/m8/feeds">Contacts</generator>
- <openSearch:totalResults>2</openSearch:totalResults>
+ <openSearch:totalResults>14</openSearch:totalResults>
  <openSearch:startIndex>1</openSearch:startIndex>
  <openSearch:itemsPerPage>25</openSearch:itemsPerPage>
- <entry gd:etag="&quot;Q3Y6cDVSLyt7I2A9XR5XGEwNRAA.&quot;">
-  <id>http://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/base/56d660e0c8cb34c</id>
-  <updated>2017-01-19T09:15:42.818Z</updated>
-  <app:edited xmlns:app="http://www.w3.org/2007/app">2017-01-19T09:15:42.818Z</app:edited>
+ <entry gd:etag="&quot;QXo-cTVSLit7I2A9XR5WGUkJRQE.&quot;">
+  <id>http://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/base/67e2e50a6ea2db</id>
+  <updated>2017-02-01T10:00:50.459Z</updated>
+  <app:edited xmlns:app="http://www.w3.org/2007/app">2017-02-01T10:00:50.459Z</app:edited>
   <category scheme="http://schemas.google.com/g/2005#kind" term="http://schemas.google.com/contact/2008#contact"/>
-  <title>Jane Doe</title>
-  <link rel="http://schemas.google.com/contacts/2008/rel#photo" type="image/*" href="https://www.google.com/m8/feeds/photos/media/easycore.sync.bridge%40gmail.com/56d660e0c8cb34c"/>
-  <link rel="self" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full/56d660e0c8cb34c"/>
-  <link rel="edit" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full/56d660e0c8cb34c"/>
+  <title>John Doah</title>
+  <link rel="http://schemas.google.com/contacts/2008/rel#photo" type="image/*" href="https://www.google.com/m8/feeds/photos/media/easycore.sync.bridge%40gmail.com/67e2e50a6ea2db"/>
+  <link rel="self" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full/67e2e50a6ea2db"/>
+  <link rel="edit" type="application/atom+xml" href="https://www.google.com/m8/feeds/contacts/easycore.sync.bridge%40gmail.com/full/67e2e50a6ea2db"/>
   <gd:name>
-   <gd:fullName>Jane Doe</gd:fullName>
-   <gd:givenName>Jane</gd:givenName>
-   <gd:familyName>Doe</gd:familyName>
+   <gd:fullName>John Doah</gd:fullName>
+   <gd:givenName>John</gd:givenName>
+   <gd:familyName>Doah</gd:familyName>
   </gd:name>
-  <gd:email rel="http://schemas.google.com/g/2005#home" address="jane.doe@example.com" primary="true"/>
-  <gd:email rel="http://schemas.google.com/g/2005#other" address="jane.doah@example.com"/>
+  <gd:organization rel="http://schemas.google.com/g/2005#other">
+   <gd:orgName>Doah Enterprises</gd:orgName>
+   <gd:orgTitle>CEO</gd:orgTitle>
+  </gd:organization>
+  <gd:email rel="http://schemas.google.com/g/2005#home" address="john.doe@example.com" primary="true"/>
+  <gd:email rel="http://schemas.google.com/g/2005#work" address="john.work@example.com"/>
+  <gd:phoneNumber rel="http://schemas.google.com/g/2005#mobile">111 222 333</gd:phoneNumber>
+  <gd:phoneNumber rel="http://schemas.google.com/g/2005#work">111 333 444</gd:phoneNumber>
+  <gd:phoneNumber rel="http://schemas.google.com/g/2005#home">111 444 555</gd:phoneNumber>
+  <gd:phoneNumber rel="http://schemas.google.com/g/2005#main">111 555 666</gd:phoneNumber>
+  <gd:structuredPostalAddress rel="http://schemas.google.com/g/2005#home">
+   <gd:formattedAddress>Street 10
+POBox
+Vyton, Praha, Praha 11000
+Czech republic</gd:formattedAddress>
+   <gd:street>Street 10</gd:street>
+   <gd:pobox>POBox</gd:pobox>
+   <gd:neighborhood>Vyton</gd:neighborhood>
+   <gd:city>Praha</gd:city>
+   <gd:region>Praha country</gd:region>
+   <gd:postcode>11000</gd:postcode>
+   <gd:country>Czech republic</gd:country>
+  </gd:structuredPostalAddress>
   <gContact:groupMembershipInfo deleted="false" href="http://www.google.com/m8/feeds/groups/easycore.sync.bridge%40gmail.com/base/6"/>
  </entry>
-
 </feed>`
 )
 
@@ -63,18 +84,9 @@ func (s *ContactSuite) TestMarshal(c *C) {
 	var err error
 	contact.Updated, err = time.Parse(time.RFC3339, "2017-01-18T16:09:09.345Z")
 	c.Assert(err, IsNil)
-	contact.Emails = []*Email{
-		{
-			Rel:     "http://schemas.google.com/g/2005#other",
-			Address: "frank.doe@example.com",
-			Primary: true,
-		},
-		{
-			Rel:     "http://schemas.google.com/g/2005#home",
-			Address: "jane.doe@example.com",
-			Primary: false,
-		},
-	}
+	mail1 := NewEmail("frank.doe@example.com", "other", true)
+	mail2 := NewEmail("jane.doe@example.com", "home", false)
+	contact.Emails = []*Email{&mail1, &mail2}
 
 	out, err := xml.Marshal(contact)
 	c.Assert(err, IsNil)
@@ -100,6 +112,23 @@ func (s *ContactSuite) TestUnmarshal(c *C) {
 	err := xml.Unmarshal([]byte(CONTACT_EXAMPLE), &into)
 	c.Assert(err, IsNil)
 	c.Assert(len(into.Contacts), Equals, 1)
-	c.Assert(into.Contacts[0].Name.FullName, Equals, "Jane Doe")
+	c.Assert(into.Contacts[0].Name.FullName, Equals, "John Doah")
 	c.Assert(len(into.Contacts[0].Emails), Equals, 2)
+	c.Assert(into.Contacts[0].Emails[0].Address, Equals, "john.doe@example.com")
+	c.Assert(into.Contacts[0].Emails[0].GetType(), Equals, "home")
+	c.Assert(into.Contacts[0].Emails[0].Primary, Equals, true)
+	c.Assert(len(into.Contacts[0].Addresses), Equals, 1)
+	c.Assert(into.Contacts[0].Addresses[0].Street, Equals, "Street 10")
+	c.Assert(into.Contacts[0].Addresses[0].POBox, Equals, "POBox")
+	c.Assert(into.Contacts[0].Addresses[0].City, Equals, "Praha")
+	c.Assert(into.Contacts[0].Addresses[0].Postcode, Equals, "11000")
+	c.Assert(into.Contacts[0].Addresses[0].Country, Equals, "Czech republic")
+
+	c.Assert(len(into.Contacts[0].Phones), Equals, 4)
+	c.Assert(into.Contacts[0].Phones[0].Number, Equals, "111 222 333")
+	c.Assert(into.Contacts[0].Phones[0].GetType(), Equals, "mobile")
+	c.Assert(into.Contacts[0].Phones[1].Number, Equals, "111 333 444")
+	c.Assert(into.Contacts[0].Phones[1].GetType(), Equals, "work")
+
+	spew.Dump(into)
 }
